6a3780d984de5066c9f23b2d2071016e976221d6
Sahid Anwar <hay.sahid@gmail.com>
2024-05-14 15:17:41 +0700

    refactor: add item card key

--- src/components/homepage/Items.vue
+++ src/components/homepage/Items.vue
@@ -3,9 +3,9 @@ import { ref } from 'vue'
 import ItemCard from '@/components/ItemCard.vue'
 
 const items = [
-    { title: 'Mobile UI Kit', description: 'Mobile UI Kit', image: 'items-1.jpg' },
-    { title: 'Online Doctor Consultation', description: 'Website UI Kit', image: 'items-2.jpg' },
-    { title: 'Banking Crypto', description: 'Mobile UI Kit', image: 'items-3.jpg' },
+    { id: 1, title: 'Mobile UI Kit', description: 'Mobile UI Kit', image: 'items-1.jpg' },
+    { id: 2, title: 'Online Doctor Consultation', description: 'Website UI Kit', image: 'items-2.jpg' },
+    { id: 3, title: 'Banking Crypto', description: 'Mobile UI Kit', image: 'items-3.jpg' },
 ]
 
 </script>
@@ -14,7 +14,8 @@ const items = [
     <div class="container px-4 mx-auto my-16 md:px-12">
         <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
         <div class="flex flex-wrap -mx-1 lg:-mx-4">
-            <ItemCard v-for="item in items" :title="item.title" :description="item.description" :image="item.image" />
+            <ItemCard v-for="item in items" :key="item.id" :title="item.title" :description="item.description"
+                :image="item.image" />
         </div>
     </div>
 </template>

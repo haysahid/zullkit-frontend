e7cb7f1302d5d201fbf01d4c0f91846a10a28f92
Sahid Anwar <hay.sahid@gmail.com>
2024-05-14 14:44:27 +0700

    feat: homepage

---
+++ src/components/homepage/Items.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+import ItemsCard from '@/components/ItemsCard.vue'
+
+const items = [
+    { title: 'Mobile UI Kit', subtitle: 'Mobile UI Kit', image: 'items-1.jpg' },
+    { title: 'Online Doctor Consultation', subtitle: 'Website UI Kit', image: 'items-2.jpg' },
+    { title: 'Banking Crypto', subtitle: 'Mobile UI Kit', image: 'items-3.jpg' },
+]
+
+</script>
+
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <ItemsCard v-for="item in items" :title="item.title" :subtitle="item.subtitle" :image="item.image" />
+        </div>
+    </div>
+</template>

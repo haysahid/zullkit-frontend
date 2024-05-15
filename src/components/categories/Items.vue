307820d9ac76b0de1307ec88975cb8a83b0b3473
Sahid Anwar <hay.sahid@gmail.com>
2024-05-15 11:07:20 +0700

    feat: fetch categories and products data

--- src/components/categories/Items.vue
+++ src/components/categories/Items.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
 import axios from "axios";
 import ItemCard from "@/components/ItemCard.vue";
 
+const router = useRoute();
 const items = ref([]);
+const category = ref({});
 
 async function getItemsData() {
   try {
-    const response = await axios.get("http://localhost:8080/api/products");
-    items.value = response.data.data.data;
+    const response = await axios.get(
+      "http://localhost:8080/api/categories?id=" +
+        router.params.id +
+        "show_product=1"
+    );
+    items.value = response.data.data.products;
+    category.value = response.data.data;
   } catch (error) {
     console.error(error);
   }
@@ -21,7 +29,7 @@ onMounted(() => {
 
 <template>
   <div class="container px-4 mx-auto my-16 md:px-12">
-    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">{{ category.name }}</h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
       <ItemCard
         v-for="item in items"
